44c03897a59d2a52aadff81fe9815db40b9c1e4e
Ade Dwi Payana <46589818+adedwip@users.noreply.github.com>
2020-03-01 18:21:57 +0700

    fix transaksi

--- main.go
+++ main.go
@@ -149,7 +149,7 @@ func handleText(message *linebot.TextMessage, replyToken string) error {
 			),
 			linebot.NewCarouselColumn(
 				imageURL2, "Transaksi", "Berisi berbagai macam informasi mengenai transaksi pelanggan",
-				linebot.NewPostbackAction("transaksi", "transakasi", "transaksi", ""),
+				linebot.NewPostbackAction("transaksi", "transaksi", "Berapa biaya bpjs saya?", ""),
 			),
 			linebot.NewCarouselColumn(
 				imageURL3, "Riwayat", "Berisi berbagai macam informasi mengenai riwayat pelanggan",
