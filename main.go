b08eabe0e2db36825d2b06152aed1e53a6ab792a
Ade Dwi Payana <46589818+adedwip@users.noreply.github.com>
2020-03-13 17:04:24 +0700

    konfirm pendaftaran

--- main.go
+++ main.go
@@ -187,19 +187,18 @@ func registerNewUser(w http.ResponseWriter, r *http.Request, userLineId string,k
 			return UserDetail{},err
 		} else {
 			log.Println("INI RESULT LINE ID dan KTP dari register : ",detail)
+			events, _ := bot.ParseRequest(r)
+			for _, event := range events {
+				if event.Type == linebot.EventTypeMessage {
+					switch _ := event.Message.(type) {
+					case *linebot.TextMessage:
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Terima kasih anda telah terdaftar"))
+					}
+				}
+			}
 			return detail,nil
 		}
 	}
-	events, err := bot.ParseRequest(r)
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch _ := event.Message.(type) {
-			case *linebot.TextMessage:
-				bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Terima kasih anda telah terdaftar"))
-			}
-		}
-	}
-	return UserDetail{}, nil
 }
 
 func updateNoKTP(w http.ResponseWriter, r *http.Request, userLineId string,ktp string) (UserDetail, error) {
