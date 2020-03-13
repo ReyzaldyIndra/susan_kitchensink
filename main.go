1a8af599e15faa01512bebe1013e22dd53c693f4
Ade Dwi Payana <46589818+adedwip@users.noreply.github.com>
2020-03-13 17:30:25 +0700

    fix confirm pendaftaran

--- main.go
+++ main.go
@@ -179,7 +179,16 @@ func registerNewUser(w http.ResponseWriter, r *http.Request, userLineId string,k
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-
+		events, _ := bot.ParseRequest(r)
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch err := event.Message.(type) {
+				case *linebot.TextMessage:
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Terima kasih anda telah terdaftar"))
+					log.Println(err)
+				}
+			}
+		}
 		return UserDetail{},err
 	} else {
 		defer resp.Body.Close()
@@ -187,16 +196,6 @@ func registerNewUser(w http.ResponseWriter, r *http.Request, userLineId string,k
 			return UserDetail{},err
 		} else {
 			log.Println("INI RESULT LINE ID dan KTP dari register : ",detail)
-			events, _ := bot.ParseRequest(r)
-			for _, event := range events {
-				if event.Type == linebot.EventTypeMessage {
-					switch err := event.Message.(type) {
-					case *linebot.TextMessage:
-						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("Terima kasih anda telah terdaftar"))
-						log.Println(err)
-					}
-				}
-			}
 			return detail,nil
 		}
 	}
